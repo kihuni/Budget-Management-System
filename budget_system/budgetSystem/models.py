5005339d6db702e62ca183dac7aaf8fe9d1b895c
kihuni <stephenkihuni55@gmail.com>
2024-04-05 07:30:35 +0300

    modified budget  and category models

--- budget_system/budgetSystem/models.py
+++ budget_system/budgetSystem/models.py
@@ -8,12 +8,18 @@ class Budget(models.Model):
     end_date = models.DateField()
     max_spending_limit = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     target_savings = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-
+    
+    def __str__(self):
+        return self.name
+    
 class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
-
+    
+    
+    def __str__(self):
+        return self.name
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,9 +30,6 @@ class Expense(models.Model):
     date = models.DateField()
     location = models.CharField(max_length=100, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-    
-    def __str__(self):
-        return f"{self.title} - {self.category.name} ({self.budget.name})" 
 
 class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -42,6 +45,4 @@ class Transfer(models.Model):
     from_budget = models.ForeignKey(Budget, related_name='transfers_sent', on_delete=models.CASCADE)
     to_budget = models.ForeignKey(Budget, related_name='transfers_received', on_delete=models.CASCADE)
     notes = models.TextField(null=True, blank=True)
-    
-    def __str__(self):
-        return f"From {self.from_budget.name} to {self.to_budget.name}" 
+
